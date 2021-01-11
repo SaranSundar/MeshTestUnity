34009d18ff4d66d1cacc55022d3fc8b9ec8fbca9
SaranSundar <saransszg@gmail.com>
2021-01-11 14:21:26 -0600

    Mesh proven to not be cpu compatible

--- Assets/Scripts/FogOfWarMesh.cs
+++ Assets/Scripts/FogOfWarMesh.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils;
+using Debug = UnityEngine.Debug;
 
 
 [RequireComponent(typeof(MeshFilter))]
@@ -17,6 +21,12 @@ public class FogOfWarMesh : MonoBehaviour
     private float size = 3.0f;
     private FieldOfViewMesh fieldOfViewMesh;
     private List<TriangleUtils.Triangle> triangleFaces;
+    private Dictionary<int, bool> markedFaces;
+    public static Stopwatch m_stopwatch = new Stopwatch();
+    private Text time1Text;
+    private Text time2Text;
+    private Text time3Text;
+    private Text fps;
 
     private Color32[] allowedTriangleFaceColors =
         {Color.black};
@@ -27,23 +37,38 @@ public class FogOfWarMesh : MonoBehaviour
         transform.position = new Vector3(0, 2, 0);
 
         // triangleFaces = MeshUtils.CreateUnitCircleMesh(mesh, allowedTriangleFaceColors, numOfTriangleFaces, size);
-        triangleFaces = MeshUtils.CreateGridMesh(mesh, allowedTriangleFaceColors, 10, 10, 1f, 1f);
+        triangleFaces = MeshUtils.CreateGridMesh(mesh, allowedTriangleFaceColors, 100, 100, 1f, 1f);
         vertices = mesh.vertices;
         colors = mesh.colors32;
         triangles = mesh.triangles;
 
+        markedFaces = new Dictionary<int, bool>();
+
         fieldOfViewMesh = GameObject.Find("FieldOfViewMesh").GetComponent<FieldOfViewMesh>();
+
+        time1Text = GameObject.Find("IntersectionsInnerTime").GetComponent<Text>();
+        time2Text = GameObject.Find("FindIntersectingTIme").GetComponent<Text>();
+        time3Text = GameObject.Find("UpdateTime").GetComponent<Text>();
+        fps = GameObject.Find("FPS").GetComponent<Text>();
     }
 
     private void Update()
 
     {
-        //FindIntersectingTriangleFaces();
-        Debug.Log(1 / Time.deltaTime);
+        var startTime = DateTime.Now;
+        FindIntersectingTriangleFaces();
+        var elapsed = (DateTime.Now - startTime).Milliseconds;
+
+        //Debug.Log("3 update: " + elapsed);
+        time3Text.text = "3 update: " + elapsed + " ms";
+        fps.text = "FPS: " + (1 / Time.deltaTime);
+        //Debug.Log(1 / Time.deltaTime);
     }
 
     void FindIntersectingTriangleFaces()
     {
+        var startTime1 = DateTime.Now;
+
         List<TriangleUtils.Triangle> playerTriangleFaces = fieldOfViewMesh.triangleFaces;
         List<TriangleUtils.Triangle> fogOfWarTriangleFaces = triangleFaces;
 
@@ -64,17 +89,66 @@ public class FogOfWarMesh : MonoBehaviour
         //     }
         // }
 
-        //KEEP TRACK IF A FACE IS MARKED
+        //replace this with checking if a color is transparent so the face is marked
+        markedFaces.Clear();
+
+        float x = 0;
+        float y = 0;
+
+        int gridPaddingHeight = 10;
+        int gridPaddingWidth = 10;
+
+        int rowStart = 0;
+        int rowEnd = rowStart + gridPaddingHeight;
+
+        int colStart = 0;
+        int colEnd = colStart + gridPaddingWidth;
+
+        var startTime = DateTime.Now;
 
         for (int r = 0; r < playerTriangleFaces.Count; r++)
         {
-            Vector3 fieldOfViewMeshTransformPosition = fieldOfViewMesh.transform.position;
-            Vector3 fogOfWarMeshTransformPosition = transform.position;
-            StartCoroutine(ThreadCallingRoutine(playerTriangleFaces, fogOfWarTriangleFaces, r,
-                fieldOfViewMeshTransformPosition, fogOfWarMeshTransformPosition));
+            for (int c = 0; c < 100; c++)
+            {
+                if (!markedFaces.ContainsKey(c) || !markedFaces[c])
+                {
+                    Vector3 fieldOfViewMeshTransformPosition = fieldOfViewMesh.transform.position;
+                    Vector3 fogOfWarMeshTransformPosition = transform.position;
+                    TriangleUtils.Triangle playerFace =
+                        applyPositionToTriangle(playerTriangleFaces[r], fieldOfViewMeshTransformPosition);
+                    TriangleUtils.Triangle fogFace =
+                        applyPositionToTriangle(fogOfWarTriangleFaces[c], fogOfWarMeshTransformPosition);
+
+                    if (TriangleUtils.IsTriangleTriangleIntersecting(playerFace, fogFace))
+                    {
+                        markedFaces.Add(c, true);
+                        ChangeTriangleFaceToTransparent(fogOfWarTriangleFaces[c]);
+                    }
+                }
+            }
         }
 
+        var elapsed = (DateTime.Now - startTime).Milliseconds;
+
+        //Debug.Log("1 intersections: " + elapsed);
+        time1Text.text = "1 intersections: " + elapsed + " ms";
+
+        //KEEP TRACK IF A FACE IS MARKED
+
+        // for (int r = 0; r < playerTriangleFaces.Count; r++)
+        // {
+        //     Vector3 fieldOfViewMeshTransformPosition = fieldOfViewMesh.transform.position;
+        //     Vector3 fogOfWarMeshTransformPosition = transform.position;
+        //     StartCoroutine(ThreadCallingRoutine(playerTriangleFaces, fogOfWarTriangleFaces, r,
+        //         fieldOfViewMeshTransformPosition, fogOfWarMeshTransformPosition));
+        // }
+
         mesh.colors32 = colors;
+
+        var elapsed1 = (DateTime.Now - startTime1).Milliseconds;
+
+        //Debug.Log("2 FindIntersectingTriangles: " + elapsed1);
+        time2Text.text = "2 FindIntersectingTriangles: " + elapsed1 + " ms";
     }
 
     public IEnumerator ThreadCallingRoutine(List<TriangleUtils.Triangle> playerTriangleFaces,
